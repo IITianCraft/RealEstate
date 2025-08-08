56432c5964b2a974301bd1fc60f287f30e360139
shravanis30 <salunkeshravani561@gmail.com>
2025-08-09 02:57:04 +0530

    demo

--- real-estate-platform/src/components/utils/axiosInstance.js
+++ real-estate-platform/src/components/utils/axiosInstance.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import { getAccessToken } from "./authTokenStore";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+ baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const token = getAccessToken();
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+ const token = getAccessToken();
+ if (token) config.headers.Authorization = `Bearer ${token}`;
+ return config;
 });
 
 export default api;
